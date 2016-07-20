b9dd9210f5f92b8411d9e0b7ea1768a3fde1b542
Thomas Minier <tminier01@gmail.com>
2016-07-20 10:18:54 +0200

    update README & improve travis script

--- travis_test.sh
+++ travis_test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-go test -coverprofile=graph.cover.out -coverpkg=./... graph
-go test -coverprofile=graph.cover.out -coverpkg=./... ./graph
-go test -coverprofile=parser.cover.out -coverpkg=./... ./parser
-go test -coverprofile=rdf.cover.out -coverpkg=./... ./rdf
-go test -coverprofile=sparql.cover.out -coverpkg=./... ./sparql
+PACKAGES="graph parser rdf sparql"
+for pkg in $PACKAGES; do
+  go test -coverprofile=$pkg.cover.out -coverpkg=./... ./$pkg
+done
 echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
